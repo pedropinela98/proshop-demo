e947cd2f2c50c98e2179aaa07c212e399795d071
Mona Lisa <pedromgpinela@gmail.com>
2023-06-05 21:24:06 +0100

    Thrid commit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,20 +1,27 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+      <Navbar bg="secondary" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">ProShop</Navbar.Brand>
+          <Navbar.Brand className="d-flex align-items-center" href="/">
+            <img className="px-2" src={logo} alt="ProShop" />
+            ProShop
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
+              <Nav.Link style={{ color: "white" }} href="/cart">
                 <FaShoppingCart />
                 Cart
               </Nav.Link>
-              <Nav.Link href="/login">Login</Nav.Link>
+              <Nav.Link style={{ color: "white" }} href="/login">
+                <FaUser />
+                Login
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
