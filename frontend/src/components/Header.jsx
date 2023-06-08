8f223911fec915b4b248c903d364d98a83809e00
Mona Lisa <pedromgpinela@gmail.com>
2023-06-08 15:04:32 +0100

    fourth commit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,27 +1,34 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import logo from "../assets/logo.png";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="secondary" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand className="d-flex align-items-center" href="/">
-            <img className="px-2" src={logo} alt="ProShop" />
-            ProShop
-          </Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand className="d-flex align-items-center">
+              <img className="px-2" src={logo} alt="ProShop" />
+              ProShop
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link style={{ color: "white" }} href="/cart">
-                <FaShoppingCart />
-                Cart
-              </Nav.Link>
-              <Nav.Link style={{ color: "white" }} href="/login">
-                <FaUser />
-                Login
-              </Nav.Link>
+              <LinkContainer to={"/cart"}>
+                <Nav.Link style={{ color: "white" }}>
+                  <FaShoppingCart />
+                  Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to={"/login"}>
+                <Nav.Link style={{ color: "white" }}>
+                  <FaUser />
+                  Login
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,11 +1,11 @@
 import { Row, Col } from "react-bootstrap";
 import products from "../products";
-import Products from "./Products";
+import Products from "../components/Products";
 
 const HomeScreen = () => {
   return (
     <>
-      <h1 style={{ color: "black" }}>Latest Products</h1>
+      <h1 style={{ color: "	#787878" }}>Latest Products</h1>
       <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3} className="mb-3">
